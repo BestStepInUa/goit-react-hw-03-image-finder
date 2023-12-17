8c051dac3cb77de728d10025fdce81ed54c249f4
BestStepInUa <beststep.in.ua@gmail.com>
2023-12-17 17:54:43 +0200

    1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,70 @@
+import { Component } from "react";
+import { Notify } from 'notiflix/build/notiflix-notify-aio.js';
+
 import Searchbar from "components/Searchbar/";
 
-const App = () => {
-  return (
-    <Searchbar />
-  );
-};
+import fetchImgs from "components/helpers/API";
 
-export default App
+import AppContainer from './App.styled'
+
+Notify.init({
+    width: '300px',
+    position: 'left-top',
+    fontSize: '16px',    
+});
+
+export default class App extends Component {
+  state = {
+    searchQuery: '',
+    page: 1
+  }
+
+  handleSearchbarSubmit = query => {
+    console.log(query)
+  };
+
+  async onSearchForm(e) {
+    const {searchQuery, page} = this.state
+    e.preventDefault();
+    searchQuery = e.currentTarget.elements.searchQuery.value.trim();
+    if (currentQuery === searchQuery) {
+      Notify.warning("Error! You are already searching for this keyword");
+      this.setState({searchQuery: ''})
+      return;
+    };
+    if (searchQuery === '') {
+      Notify.warning("Error! You must specify a keyword to search for.");
+      this.setState({searchQuery: ''})
+      return;
+    };
+    try {
+      page = 1;
+      console.log(`searchQuery: ${searchQuery}, page before fetch: ${page}`);
+      let data = await fetchImgs(searchQuery, page);
+      console.log('Hits: ', data.hits);
+      if (data.hits && data.hits.length > 0) {
+        Notify.success(`Hooray! We found ${data.totalHits} images.`)
+        this.setState({searchQuery: ''})
+        page++;
+        currentPage = page;
+        currentQuery = searchQuery;
+        console.log(`searchQuery: ${searchQuery}, page after fetch: ${page}`);
+      } else {
+        Notify.failure("Sorry, there are no images matching your search query. Please try again");
+        this.setState({searchQuery: ''})
+        searchQuery = currentQuery;
+        page = currentPage;
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+
+    render() {
+      return (
+        <AppContainer>
+          <Searchbar onSubmit={this.handleSearchbarSubmit} />
+        </AppContainer>
+      )
+    }
+  }
+}
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -4,11 +4,29 @@ export default class Searchbar extends Component {
      state = {  
         query: ''  
     }
+
+    handelChange = evt => {
+        const { name, value } = evt.currentTarget        
+        this.setState({[name]: value})
+    }
+
+    handelFormSubmit = evt => {
+        const { query } = this.state
+        evt.preventDefault()       
+        this.props.onSubmit(query)
+        this.reset()
+    }
+
+    reset = () => {
+        this.setState({query: ''})
+    }
     
     render() {
+        const { query } = this.state
+       
         return (
             <header className="Searchbar">
-                <form className="SearchForm">
+                <form className="SearchForm" onSubmit={this.handelFormSubmit}>
                     <button type="submit" className="SearchForm-button">
                     <span className="SearchForm-button-label">Search</span>
                     </button>
@@ -16,8 +34,11 @@ export default class Searchbar extends Component {
                     <input
                     className="SearchForm-input"
                     type="text"
-                    autocomplete="off"
-                    autofocus
+                    name="query"
+                    value={query}
+                    onChange={this.handelChange}    
+                    autoComplete="off"
+                    autoFocus
                     placeholder="Search images and photos"
                     />
                 </form>
