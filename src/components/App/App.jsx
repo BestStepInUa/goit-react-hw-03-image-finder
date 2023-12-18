65fe937a029a7611753ace221c5e70e1a4e9761e
BestStepInUa <beststep.in.ua@gmail.com>
2023-12-18 11:36:39 +0200

    caught the loop

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -12,52 +12,95 @@ Notify.init({
     position: 'left-top',
     fontSize: '16px',    
 });
-
+// let searchQuery;
+// let currentQuery;
+// let page;
+// let currentPage;
+// const perPage = 40;
 export default class App extends Component {
   state = {
-    searchQuery: '',
-    page: 1
+    query: '',
+    currentQuery: '',
+    page: null,
+    currentPage: null,
+    hits: []
+  }
+
+  async componentDidUpdate(prevProps, prevState) {
+    const { query, currentQuery, page, currentPage } = this.state
+     if (currentQuery === query) {
+       Notify.warning("Error! You are already searching for this keyword");
+       this.setState({query: ''})
+       return;
+      }
+      if (query === '') {
+      Notify.warning("Error! You must specify a keyword to search for.");
+      this.setState({query: ''})
+      return;
+      }
+      try {
+        this.setState({page: 1})
+        console.log(`searchQuery: ${query}, page before fetch: ${page}`);
+        let data = await fetchImgs(query, page);
+        console.log('Hits: ', data.hits);
+        if (data.hits && data.hits.length > 0) {
+          Notify.success(`Hooray! We found ${data.totalHits} images.`)
+          this.setState({query: ''})
+          this.setState(prevState => page === prevState.page +1)
+          this.setState({currentPage: page})
+          this.setState({currentQuery: query})
+          console.log(`searchQuery: ${query}, page after fetch: ${page}`);
+        } else {
+          Notify.failure("Sorry, there are no images matching your search query. Please try again");
+          this.setState({ query: '' })
+          this.setState({ query: currentQuery })
+          this.setState({page: currentPage})
+        }
+      } catch (error) {
+        console.error(error.message);
+      } 
   }
 
   handleSearchbarSubmit = query => {
     console.log(query)
+    this.setState({query})
   };
 
-  async onSearchForm(e) {
-    const {searchQuery, page} = this.state
-    e.preventDefault();
-    searchQuery = e.currentTarget.elements.searchQuery.value.trim();
-    if (currentQuery === searchQuery) {
-      Notify.warning("Error! You are already searching for this keyword");
-      this.setState({searchQuery: ''})
-      return;
-    };
-    if (searchQuery === '') {
-      Notify.warning("Error! You must specify a keyword to search for.");
-      this.setState({searchQuery: ''})
-      return;
-    };
-    try {
-      page = 1;
-      console.log(`searchQuery: ${searchQuery}, page before fetch: ${page}`);
-      let data = await fetchImgs(searchQuery, page);
-      console.log('Hits: ', data.hits);
-      if (data.hits && data.hits.length > 0) {
-        Notify.success(`Hooray! We found ${data.totalHits} images.`)
-        this.setState({searchQuery: ''})
-        page++;
-        currentPage = page;
-        currentQuery = searchQuery;
-        console.log(`searchQuery: ${searchQuery}, page after fetch: ${page}`);
-      } else {
-        Notify.failure("Sorry, there are no images matching your search query. Please try again");
-        this.setState({searchQuery: ''})
-        searchQuery = currentQuery;
-        page = currentPage;
-      }
-    } catch (error) {
-      console.error(error.message);
-    }
+  // async onSearchForm(e) {
+  //   const {searchQuery, page} = this.state
+  //   e.preventDefault();
+  //   searchQuery = e.currentTarget.elements.searchQuery.value.trim();
+  //   if (currentQuery === searchQuery) {
+  //     Notify.warning("Error! You are already searching for this keyword");
+  //     this.setState({searchQuery: ''})
+  //     return;
+  //   };
+  //   if (searchQuery === '') {
+  //     Notify.warning("Error! You must specify a keyword to search for.");
+  //     this.setState({searchQuery: ''})
+  //     return;
+  //   };
+  //   try {
+  //     page = 1;
+  //     console.log(`searchQuery: ${searchQuery}, page before fetch: ${page}`);
+  //     let data = await fetchImgs(searchQuery, page);
+  //     console.log('Hits: ', data.hits);
+  //     if (data.hits && data.hits.length > 0) {
+  //       Notify.success(`Hooray! We found ${data.totalHits} images.`)
+  //       this.setState({searchQuery: ''})
+  //       page++;
+  //       currentPage = page;
+  //       currentQuery = searchQuery;
+  //       console.log(`searchQuery: ${searchQuery}, page after fetch: ${page}`);
+  //     } else {
+  //       Notify.failure("Sorry, there are no images matching your search query. Please try again");
+  //       this.setState({searchQuery: ''})
+  //       searchQuery = currentQuery;
+  //       page = currentPage;
+  //     }
+  //   } catch (error) {
+  //     console.error(error.message);
+  //   }
 
     render() {
       return (
@@ -67,4 +110,3 @@ export default class App extends Component {
       )
     }
   }
-}
--- src/components/helpers/API/API.js
+++ src/components/helpers/API/API.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-export default async function fetchImgs(searchQuery, page) {
+export default async function fetchImgs(query, page) {
     const BASE_API_URL = 'https://pixabay.com/api/';
     const API_KEY = '40254298-f0122bcc19424dfed523c5016';        
          
     const params = new URLSearchParams({
         key: API_KEY,
-        q: searchQuery,
+        q: query,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
