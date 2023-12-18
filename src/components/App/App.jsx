52c713fa01128ab6b3af2d7634ca788538b2d6d9
BestStepInUa <beststep.in.ua@gmail.com>
2023-12-18 15:57:56 +0200

    Loop error

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,6 +7,7 @@ import fetchImgs from "components/helpers/API";
 
 import AppContainer from './App.styled'
 import ImageGallery from "components/ImageGallery";
+import LoadMoreButton from "components/LoadMoreButton";
 
 Notify.init({
     width: '300px',
@@ -16,53 +17,80 @@ Notify.init({
 export default class App extends Component {
 
   state = {
-    query: '',    
-    page: 1,    
-    hits: []
+    query: '',
+    page: 1,
+    hits: [],
+    loadMore: false
   }
 
   async componentDidUpdate(_, prevState) {
     const { query, page } = this.state
 
-     if (query === '') {
+    if (query === '') {
       Notify.warning("Error! You must specify a keyword to search for.")
       return
     }
-    console.log(prevState.query);
-   
+       
     if (page !== prevState.page || query !== prevState.query) {
       try {
         let data = await fetchImgs(query, page)
         console.log('Hits:', data.hits);
         if (data.hits && data.hits.length > 0) {
           Notify.success(`Hooray! We found ${data.totalHits} images.`)
-          this.setState({hits: data.hits})
+          this.setState({ hits: data.hits })
+          this.setState((prevState) => ({ page: prevState.page + 1 }))
+          this.setState({loadMore: true})
+
         } else {
           Notify.failure("Sorry, there are no images matching your search query. Please try again.")
         }
       }
-        catch (error) {
-          console.error(error.message);
-        }
-      
+      catch (error) {
+        console.error(error.message);
       }
+      
     }
+  }
   
   handleSearchbarSubmit = query => {
     console.log('query:', query)
-    if (query === this.state.query) {      
+    if (query === this.state.query) {
       Notify.warning("Error! You are already searching for this keyword.")
       return
-    }   
-    this.setState({query})
+    }
+    this.setState({ query })
   };
 
-    render() {
+  async handleLoadMoreButton() {
+    const { query, page} = this.state
+    console.log('Load more');
+    try {
+      let data = await fetchImgs(query, page)
+      console.log('Hits:', data.hits);
+      if (data.hits && data.hits.length > 0) {
+        this.setState((prevState) => ({ hits: [...prevState.hits, ...data.hits] }))
+        this.setState((prevState) => ({ page: prevState.page + 1 }))
+
+      } else {
+        Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+      }
+    }
+    catch (error) {
+      console.error(error.message);
+    }
+      
+  }
+
+  render() {
+      const {hits, loadMore} = this.state
       return (
         <AppContainer>
           <Searchbar onSubmit={this.handleSearchbarSubmit} />
-          <ImageGallery hits={this.state.hits} />
-        </AppContainer>
+          <ImageGallery hits={hits} />
+          {loadMore > 0 && <LoadMoreButton onLoadMoreButtonClick={this.handleLoadMoreButton} />} 
+       </AppContainer>
+          
+        
       )
     }
   }
